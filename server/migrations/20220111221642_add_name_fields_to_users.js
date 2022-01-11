c7b10d56c98df0ed9d349401a4f5c1c45b7a0864
Aleksandr T <tychkovas@yandex.ru>
2022-01-11 22:21:18 +0400

    feat(migrations): add name fields to Users

--- server/migrations/20220111221642_add_name_fields_to_users.js
+++ server/migrations/20220111221642_add_name_fields_to_users.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.table('users', function (table) {
+       table.string('firstName');
+       table.string('lastName');
+     })  
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = (knex) => (
+   knex.schema.table('users', (table) => {
+       table.dropColumn('firstName');
+       table.dropColumn('lastName');
+     }) 
+);
