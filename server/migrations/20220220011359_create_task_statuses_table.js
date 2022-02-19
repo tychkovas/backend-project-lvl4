ca1885e83c798d03b092002cf8812e61df0b2f57
Aleksandr T <tychkovas@yandex.ru>
2022-02-20 02:07:46 +0400

    feat(migrate): add create_task_statuses_table

---
+++ server/migrations/20220220011359_create_task_statuses_table.js
@@ -0,0 +1,11 @@
+
+exports.up = (knex) => (
+  knex.schema.createTable('task_statuses', (table) => {
+    table.increments('id').primary();
+    table.string('name');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  })
+);
+
+exports.down = (knex) => knex.schema.dropTable('task_statuses');
