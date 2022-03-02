b912d6ef82365b937edc9a48ae75f80caf4806e7
Aleksandr T <tychkovas@yandex.ru>
2022-03-03 01:18:28 +0400

    feat(model): add Task model sketch

---
+++ server/models/Task.js
@@ -0,0 +1,26 @@
+import { Model } from 'objection';
+
+import objectionUnique from 'objection-unique';
+
+const unique = objectionUnique({ fields: ['email'] });
+
+export default class Task extends unique(Model) {
+  static get tableName() {
+    return 'tasks';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['name', 'statusId', 'creatorId'],
+      properties: {
+        id: { type: 'integer' },
+        name: { type: 'string', minLength: 1 },
+        description: { type: 'string' },
+        statusId: { type: 'integer' },
+        creatorId: { type: 'integer' },
+        executorId: { type: 'integer' },
+      },
+    };
+  }
+}
