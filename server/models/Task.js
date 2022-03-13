f3e6e02e2be30eefcb2479bf282af04ceb1cad76
Aleksandr T <tychkovas@yandex.ru>
2022-03-14 01:37:55 +0400

    refactor(model Tasks): add BaseModel feat: executorId field may be missing

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,10 +1,10 @@
-import { Model } from 'objection';
-
 import objectionUnique from 'objection-unique';
+
+import BaseModel from './BaseModel.js';
 
 const unique = objectionUnique({ fields: ['email'] });
 
-export default class Task extends unique(Model) {
+export default class Task extends unique(BaseModel) {
   static get tableName() {
     return 'tasks';
   }
@@ -13,13 +13,14 @@ export default class Task extends unique(Model) {
     return {
       type: 'object',
       required: ['name', 'statusId', 'creatorId'],
+
       properties: {
         id: { type: 'integer' },
         name: { type: 'string', minLength: 1 },
         description: { type: 'string' },
         statusId: { type: 'integer' },
         creatorId: { type: 'integer' },
-        executorId: { type: 'integer' },
+        executorId: { type: ['integer', 'null'] },
       },
     };
   }
