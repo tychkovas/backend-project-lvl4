bca64ba6dd980604fcb380d1b30903c35f2d7abe
Александр Сергеевич. Тычков <tychkov.as@skitlab.ru>
2022-03-14 18:22:55 +0400

    fix(migrate): add description in create_tasks_table

--- server/migrations/20220314131006_create_tasks_table.js
+++ server/migrations/20220314131006_create_tasks_table.js
@@ -4,6 +4,7 @@ exports.up = (knex) => (
   knex.schema.createTable('tasks', (table) => {
     table.increments('id').primary();
     table.string('name');
+    table.string('description');
     table.integer('statusId');
     table.integer('creatorId');
     table.integer('executorId');
