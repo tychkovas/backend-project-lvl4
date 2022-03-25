8e3196268eb1894e5f46826d81e7a5fe5f52d58a
Aleksandr T <tychkovas@yandex.ru>
2022-03-25 07:04:45 +0400

    feat(routes tasks): add post 'createTasks'

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,4 +1,4 @@
-// import i18next from 'i18next';
+import i18next from 'i18next';
 
 export default (app) => {
   app
@@ -20,5 +20,29 @@ export default (app) => {
       const users = await app.objection.models.user.query();
       reply.render('tasks/new', { task, statuses, users });
       return reply;
+    })
+    .post('/tasks', { name: 'createTasks', preValidation: app.authenticate }, async (req, reply) => {
+      const statuses = await app.objection.models.taskStatus.query();
+      const users = await app.objection.models.user.query();
+      try {
+        const { data } = req.body;
+        data.creatorId = req.session.get('userId');
+        req.log.info(`/tasks edit user = ${JSON.stringify(data)}`);
+
+        const task = await app.objection.models.taskStatus
+          .fromJson(data);
+
+        await app.objection.models.task.query().insert(task);
+
+        req.flash('info', i18next.t('flash.tasks.create.success'));
+        reply.redirect(app.reverse('tasks'));
+        return reply;
+      } catch ({ data }) {
+        req.flash('error', i18next.t('flash.tasks.create.error'));
+        reply.render('tasks/new', {
+          task: req.body.data, statuses, users, errors: data,
+        });
+        return reply;
+      }
     });
 };
