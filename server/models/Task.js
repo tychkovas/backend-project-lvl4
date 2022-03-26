9c4e96adc19eaf99c89e4e48b191e3a633ed12a7
Aleksandr T <tychkovas@yandex.ru>
2022-03-26 14:01:46 +0400

    feat(models tasks):add requirement for ids - minimum values

--- server/models/Task.js
+++ server/models/Task.js
@@ -18,9 +18,18 @@ export default class Task extends unique(BaseModel) {
         id: { type: 'integer' },
         name: { type: 'string', minLength: 1 },
         description: { type: 'string' },
-        statusId: { type: 'integer' },
-        creatorId: { type: 'integer' },
-        executorId: { type: ['integer', 'null'] },
+        statusId: {
+          type: 'integer',
+          minimum: 1,
+        },
+        creatorId: {
+          type: 'integer',
+          minimum: 1,
+        },
+        executorId: {
+          type: ['integer', 'null'],
+          minimum: 1,
+        },
       },
     };
   }
