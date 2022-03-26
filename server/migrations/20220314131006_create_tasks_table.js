9aa797d392b447a060ebb14bbbe656d6527d3fef
Aleksandr T <tychkovas@yandex.ru>
2022-03-26 11:59:59 +0400

    fix(migrate):  refactor create_tasks_table add references in tables

--- server/migrations/20220314131006_create_tasks_table.js
+++ server/migrations/20220314131006_create_tasks_table.js
@@ -5,9 +5,9 @@ exports.up = (knex) => (
     table.increments('id').primary();
     table.string('name');
     table.string('description');
-    table.integer('statusId');
-    table.integer('creatorId');
-    table.integer('executorId');
+    table.integer('status_id').references('id').inTable('task_statuses');
+    table.integer('creator_id').references('id').inTable('users')
+    table.integer('executor_id').references('id').inTable('users');
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   })
