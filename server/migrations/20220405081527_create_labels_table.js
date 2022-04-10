6651c9e1cebf8755d325c10461c627a0eadcc107
Aleksandr T <tychkovas@yandex.ru>
2022-04-10 23:15:49 +0400

    refactor(migrations): separate creating tasks_labels table

--- server/migrations/20220405081527_create_labels_table.js
+++ server/migrations/20220405081527_create_labels_table.js
@@ -7,27 +7,6 @@ exports.up = (knex) => (
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   })
-  .createTable('tasks_labels', (table) => {
-    table.increments('id').primary();
-
-    table
-      .integer('task_id')
-      .unsigned()
-      .references('id')
-      .inTable('tasks')
-      .onDelete('CASCADE')
-      .index()
-   
-    table
-     .integer('label_id')
-     .unsigned()
-     .references('id')
-     .inTable('labels')
-     .onDelete('CASCADE')
-     .index()
-
-  })
 );
 
-exports.down = (knex) => knex.schema.dropTable('labels')
-  .dropTable('tasks_labels');
+exports.down = (knex) => knex.schema.dropTable('labels');
