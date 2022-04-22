3cc59e31e366713c5a1006a50b59c265a3bba629
Александр Сергеевич. Тычков <tychkov.as@skitlab.ru>
2022-04-22 13:27:59 +0400

    refactor(webpack): remove babel use ES module syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,13 @@
-import path from 'path';
+// @ts-check
+
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 const mode = process.env.NODE_ENV || 'development';
 
-module.exports = {
+export default {
   mode,
-  devtool: 'source-map',
-  // entry: path.join(__dirname, 'src', 'index.js'),
-  output: {
-    path: path.join(__dirname, 'dist', 'public'),
-    // publicPath: '/assets/',
-  },
-  devServer: {
-    host: 'localhost',
-    // contentBase: path.join(__dirname, 'dist', 'public'),
-    publicPath: '/assets/',
-    port: 8080,
-    compress: true,
-  },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
